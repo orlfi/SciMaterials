ef6a26b9420d5e7cd19ddbe5a7a2674644f19788
Дмитрий <semu-dima@yandex.ru>
2022-09-29 09:54:57 +0300

    added empty data seeder

---
+++ Data/SciMateraials.DAL/InitializationDb/Implementation/DataSeeder.cs
@@ -0,0 +1,16 @@
+﻿using SciMaterials.DAL.Contexts;
+
+namespace SciMaterials.DAL.InitializationDb.Implementation
+{
+    public static class DataSeeder
+    {
+        public static async Task Seed(SciMaterialsContext db, CancellationToken cancel = default)
+        {
+            await using var transaction = await db.Database.BeginTransactionAsync(cancel).ConfigureAwait(false);
+
+
+
+            await transaction.CommitAsync(cancel).ConfigureAwait(false);
+        }
+    }
+}
