09409dc13b0a7921fc875383363213c093b53ebf
Orlfi <orlfi@mail.ru>
2022-09-29 09:51:06 +0300

    Add ratings entity

--- Data/SciMateraials.DAL/Models/User.cs
+++ Data/SciMateraials.DAL/Models/User.cs
@@ -6,12 +6,14 @@ public class User : NamedModel
 {
     public string Email { get; set; } = string.Empty;
 
-    public virtual ICollection<Comment> Comments { get; set; }
-    public virtual ICollection<File> Files { get; set; }
+    public ICollection<Comment> Comments { get; set; }
+    public ICollection<File> Files { get; set; }
+    public ICollection<Rating> Ratings { get; set; }
 
     public User()
     {
         Comments = new HashSet<Comment>();
         Files = new HashSet<File>();
+        Ratings = new HashSet<Rating>();
     }
 }
