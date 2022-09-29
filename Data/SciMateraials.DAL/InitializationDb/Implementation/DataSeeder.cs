ec36767296b584be77c10f8aa15cd56b0ef5fa43
Дмитрий <semu-dima@yandex.ru>
2022-09-29 23:13:23 +0300

    added code

--- Data/SciMateraials.DAL/InitializationDb/Implementation/DataSeeder.cs
+++ Data/SciMateraials.DAL/InitializationDb/Implementation/DataSeeder.cs
@@ -8,7 +8,53 @@ namespace SciMaterials.DAL.InitializationDb.Implementation
         {
             await using var transaction = await db.Database.BeginTransactionAsync(cancel).ConfigureAwait(false);
 
+            if (!db.Files.Any())
+            {
+                await db.Files.AddRangeAsync().ConfigureAwait(false);
+                await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+            }
 
+            if (!db.Users.Any())
+            {
+                await db.Users.AddRangeAsync().ConfigureAwait(false);
+                await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+            }
+
+            if (!db.Categories.Any())
+            {
+                await db.Categories.AddRangeAsync().ConfigureAwait(false);
+                await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+            }
+
+            if (!db.Comments.Any())
+            {
+                await db.Comments.AddRangeAsync().ConfigureAwait(false);
+                await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+            }
+
+            if (!db.FileGroups.Any())
+            {
+                await db.FileGroups.AddRangeAsync().ConfigureAwait(false);
+                await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+            }
+
+            if (!db.Tags.Any())
+            {
+                await db.Tags.AddRangeAsync().ConfigureAwait(false);
+                await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+            }
+
+            if (!db.Ratings.Any())
+            {
+                await db.Ratings.AddRangeAsync().ConfigureAwait(false);
+                await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+            }
+
+            if (!db.ContentTypes.Any())
+            {
+                await db.ContentTypes.AddRangeAsync().ConfigureAwait(false);
+                await db.SaveChangesAsync(cancel).ConfigureAwait(false);
+            }
 
             await transaction.CommitAsync(cancel).ConfigureAwait(false);
         }
