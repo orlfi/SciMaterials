f22eca63bcbe7fa5ace11d9a5d342d2565067f2b
AlexanderMeshchaninov <alexander_freeman85@mail.ru>
2022-10-04 22:27:17 +0300

    - Вынес конфигурацию строки подключения к бд и первоначальные настройки админа в секреты; - Добавил новый отдельный проект DAL.AUTH.

---
+++ Data/SciMaterials.DAL.AUTH/Context/AuthDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SciMaterials.DAL.AUTH.Context;
+
+public class AuthDbContext : IdentityDbContext<IdentityUser>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
+}
--- Data/SciMaterials.DAL.AUTH/InitializationDb/AuthDbInitializer.cs
+++ Data/SciMaterials.DAL.AUTH/InitializationDb/AuthDbInitializer.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 
-namespace SciMaterials.DAL.InitializationDb.Implementation;
+namespace SciMaterials.DAL.AUTH.InitializationDb;
 
 public static class AuthDbInitializer
 {
@@ -16,8 +16,9 @@ public static class AuthDbInitializer
         RoleManager<IdentityRole> roleManager,
         IConfiguration configuration)
     {
-        string superAdminEmail = configuration.GetSection("SuperAdminSettings:Login").Value;
-        string superAdminPassword = configuration.GetSection("SuperAdminSettings:Password").Value;
+        var adminSettings = configuration.GetSection("AdminSettings");
+        string adminEmail = adminSettings["login"];
+        string adminPassword = adminSettings["password"];
         
         //Роль админа
         if (await roleManager.FindByNameAsync("admin") is null)
@@ -32,15 +33,15 @@ public static class AuthDbInitializer
         }
         
         //Супер админ
-        if (await userManager.FindByNameAsync(superAdminEmail) is null)
+        if (await userManager.FindByNameAsync(adminEmail) is null)
         {
             var superAdmin = new IdentityUser()
             {
-                Email = superAdminEmail, 
-                UserName = superAdminEmail
+                Email = adminEmail, 
+                UserName = adminEmail
             };
             
-            var identityResult = await userManager.CreateAsync(superAdmin, superAdminPassword);
+            var identityResult = await userManager.CreateAsync(superAdmin, adminPassword);
                 
             if (identityResult.Succeeded)
             {
