3edf8cfdc9e66c6fb5d4c8215b83232349fa77c9
Дмитрий <semu-dima@yandex.ru>
2022-10-04 09:47:08 +0300

    added extensions

---
+++ Tests/SciMaterials.ConsoleTests/Extensions/DbContextServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using SciMaterials.DAL.Contexts;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DbContextServiceCollectionExtension
+    {
+        public static IServiceCollection AddContextMultipleProviders(this IServiceCollection services, HostBuilderContext context)
+        {
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+
+            var defaultProvider = context.Configuration.GetValue("Provider", "SqlServer");
+
+            services.AddDbContext<SciMaterialsContext>(options => _ = defaultProvider switch
+            {
+                "SqlServer" => options.UseSqlServer(context.Configuration.GetConnectionString("SqlServerConnectionString"), 
+                    optionsBuilder => optionsBuilder.MigrationsAssembly("SciMaterials.MsSqlServerMigrations")),
+                "PostgreSQL" => options.UseNpgsql(context.Configuration.GetConnectionString("PostgreSQLConnectionString"), 
+                    optionsBuilder => optionsBuilder.MigrationsAssembly("SciMaterials.PostgresqlMigrations")),
+                _ => throw new Exception($"Unsupported provider: {defaultProvider}")
+            });
+
+            return services;
+        }
+    }
+}
