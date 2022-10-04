2de8762b133b2030fcfdd5514f4ac4be2b106349
Дмитрий <semu-dima@yandex.ru>
2022-10-04 09:47:51 +0300

    registered service, configured

--- Tests/SciMaterials.ConsoleTests/Program.cs
+++ Tests/SciMaterials.ConsoleTests/Program.cs
@@ -1,19 +1,18 @@
 ﻿#region usings
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SciMaterials.DAL.Contexts;
 using SciMaterials.DAL.InitializationDb.Implementation;
 using SciMaterials.DAL.InitializationDb.Interfaces;
 #endregion
 
 static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
-    .ConfigureServices(ConfigureServices);
-
+    .ConfigureServices(ConfigureServices)
+    .ConfigureAppConfiguration(app => app.AddJsonFile("appsettings.json"));
 
 static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
 {
-    services.AddDbContext<SciMaterialsContext>(options => options.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=SciMaterials;Integrated Security=True"));
+    services.AddContextMultipleProviders(context);
     services.AddTransient<IDbInitializer, DbInitializer>();
 }
 
