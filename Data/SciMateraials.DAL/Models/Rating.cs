c1d02761a0d12a134dd58f75b6da0b5ed4628443
Orlfi <orlfi@mail.ru>
2022-10-06 10:01:25 +0300

    Add filegroup to ratings

--- Data/SciMateraials.DAL/Models/Rating.cs
+++ Data/SciMateraials.DAL/Models/Rating.cs
@@ -4,10 +4,12 @@ namespace SciMaterials.DAL.Models;
 
 public partial class Rating : BaseModel
 {
-    public Guid FileId { get; set; }
+    public Guid? FileId { get; set; }
+    public Guid? FileGroupId { get; set; }
     public Guid UserId { get; set; }
     public int RatingValue { get; set; }
 
-    public File File { get; set; } = null!;
+    public File? File { get; set; } = null!;
+    public File? FileGroup { get; set; } = null!;
     public User User { get; set; } = null!;
 }
