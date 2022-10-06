6ca9077b5872f0743175ae0192fa31226f3a8c2d
AlexanderMeshchaninov <alexander_freeman85@mail.ru>
2022-10-06 20:22:45 +0300

    Внес дополнительные изменения в аутентификацию: - Jwt токены; - Вынес параметры строки подключения и инициализацию admin в секреты. - Поправил код.

--- UI/SciMaterials.AUTH/Program.cs
+++ UI/SciMaterials.AUTH/Program.cs
@@ -1,4 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SciMaterials.Auth.Registrations;
 using SciMaterials.DAL.AUTH.Context;
 using SciMaterials.DAL.AUTH.InitializationDb;
@@ -16,8 +20,39 @@ public class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        
+        builder.Services.AddSwaggerGen(opt =>
+        {
+            opt.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Сервис аутентификации SciMaterials",
+                Version = "v1.1",
+            });
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Description = "JWT Authorization header using the Bearer scheme(Example: 'Bearer 12345abcdef')",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    }, 
+                    Array.Empty<string>()
+                }
+            });
+        });
 
+        builder.Services.RegisterAuthUtils();
         builder.Services.RegisterMySqlProvider(builder.Configuration);
         builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
         {
@@ -34,6 +69,30 @@ public class Program
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
         
         builder.Services.AddHttpContextAccessor();
+        
+        builder.Services.AddAuthentication(x => 
+            {
+                x.DefaultAuthenticateScheme =
+                    JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme =
+                    JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                        builder.Configuration.GetSection("SecretTokenKey:Key").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+        builder.Services.AddAuthorization();
         
         var app = builder.Build();
 
