063c9449bee16307b0c97f48cad1c6c50abe0fa7
Orlfi <orlfi@mail.ru>
2022-10-10 09:59:16 +0300

    Add hash property

--- Data/SciMateraials.DAL/Models/File.cs
+++ Data/SciMateraials.DAL/Models/File.cs
@@ -6,11 +6,12 @@ public class File : Resource
 {
     public string? Url { get; set; }
     public long Size { get; set; }
+    public string? Hash { get; set; }
     public Guid? ContentTypeId { get; set; }
     public Guid? FileGroupId { get; set; }
 
     public ContentType? ContentType { get; set; }
     public FileGroup? FileGroup { get; set; }
 
-    public File():base(){}
+    public File() : base() { }
 }
