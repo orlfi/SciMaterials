eb3b463688cec390c21bd87b1dcad9ba780c8771
Tarxos <ftarxos@gmail.com>
2022-10-11 15:54:19 +0300

    extract sign in and sign up formforms models

---
+++ UI/SciMaterials.UI.BWASM/Models/SignUpForm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SciMaterials.UI.BWASM.Models;
+
+public class SignUpForm
+{
+    [Required]
+    [StringLength(255, ErrorMessage = "Name length can't be more than 255.")]
+    public string? Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [Required]
+    [StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
+    public string? Password { get; set; }
+}
