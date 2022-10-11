4d2e38f0f92b1276813f98b5be67f03b713fe7d3
Tarxos <ftarxos@gmail.com>
2022-10-11 19:39:56 +0300

    update nuget packages, add blazored local storage

--- UI/SciMaterials.UI.BWASM/Program.cs
+++ UI/SciMaterials.UI.BWASM/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services
     .AddAuthorizationCore()
