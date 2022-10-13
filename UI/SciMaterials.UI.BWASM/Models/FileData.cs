135466991b20e53c0fb79a4e23a9eb0a92c83e6b
Tarxos <ftarxos@gmail.com>
2022-10-13 23:46:02 +0300

    prototype file upload fill behavior and state, add test file upload data model, add link to file upload view from storage view

---
+++ UI/SciMaterials.UI.BWASM/Models/FileData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace SciMaterials.UI.BWASM.Models;
+
+public class FileUploadData
+{
+    public FileUploadData(IBrowserFile file)
+    {
+        File = file;
+        FileName = file.Name;
+    }
+
+    public IBrowserFile File { get; }
+    public string FileName { get; set; }
+    public string? Category { get; set; }
+}
