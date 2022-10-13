a72221e14f07ccc46cba9de2dbd827c3ad433d52
AlexanderMeshchaninov <alexander_freeman85@mail.ru>
2022-10-13 22:23:35 +0300

    Auth last update 13.10.22 - 22:23

--- Services/SciMaterials.Contracts.API/Mappings/FileProfile.cs
+++ Services/SciMaterials.Contracts.API/Mappings/FileProfile.cs
@@ -2,7 +2,7 @@
 using SciMaterials.Contracts.API.DTO.Files;
 using File = SciMaterials.DAL.Models.File;
 
-namespace SciMaterials.UI.MVC.API.Mappings;
+namespace SciMaterials.Contracts.API.Mappings;
 
 public class FileProfile:Profile
 {
--- Services/SciMaterials.Contracts/Exceptions/ApiException.cs
+++ Services/SciMaterials.Contracts/Exceptions/ApiException.cs
@@ -3,8 +3,6 @@ namespace SciMaterials.Contracts.Exceptions;
 public class ApiException : Exception
 {
     public int Code { get; set; }
-
     public ApiException(int code) : this(code, $"Exception with error code {code}") { }
-
     public ApiException(int code, string message) : base(message) => Code = code;
 }
