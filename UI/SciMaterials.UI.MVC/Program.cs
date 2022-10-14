b31a2b0b58bffb174748083eb0002e5232135d27
Дмитрий <semu-dima@yandex.ru>
2022-10-14 09:57:25 +0300

    calling the UseInitializationDbAsync method and cleaning the code

--- UI/SciMaterials.UI.MVC/Program.cs
+++ UI/SciMaterials.UI.MVC/Program.cs
@@ -1,10 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using SciMaterials.DAL.Contexts;
-using SciMaterials.DAL.InitializationDb.Interfaces;
+using SciMaterials.DAL.Extensions;
 using SciMaterials.Services.API.Extensions;
 using SciMaterials.UI.MVC.API.Middlewares;
 using SciMaterials.UI.MVC.API.Extensions;
-using SciMaterials.Domain.Extensions;
 using SciMaterials.Services.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,17 +22,7 @@ builder.Services.AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
-await using (var scope = app.Services.CreateAsyncScope())
-{
-    if (builder.Configuration["DbProvider"].Equals("SQLite"))
-    {
-        var context = scope.ServiceProvider.GetRequiredService<SciMaterialsContext>();
-        await context.Database.MigrateAsync().ConfigureAwait(false);
-    }
-    
-    var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
-    await dbInitializer.InitializeDbAsync(removeAtStart: true).ConfigureAwait(false);
-}
+await app.UseInitializationDbAsync(builder.Configuration);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
