baf3a470158da65e4c4ec84f757359066ff60390
Дмитрий <semu-dima@yandex.ru>
2022-10-14 09:32:20 +0300

    added an extension class with an extension method for the application

---
+++ Data/SciMaterials.DAL/Extensions/ApplicationExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SciMaterials.DAL.Contexts;
+using SciMaterials.DAL.InitializationDb.Interfaces;
+
+namespace SciMaterials.DAL.Extensions
+{
+    public static class ApplicationExtension
+    {
+        public static async Task<IApplicationBuilder> UseInitializationDbAsync(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            await using var scope = app.ApplicationServices.CreateAsyncScope();
+
+            if (configuration["DbProvider"].Equals("SQLite"))
+            {
+                var context = scope.ServiceProvider.GetRequiredService<SciMaterialsContext>();
+                await context.Database.MigrateAsync().ConfigureAwait(false);
+            }
+
+            var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+            await dbInitializer.InitializeDbAsync(removeAtStart: true).ConfigureAwait(false);
+
+            return app;
+        }
+    }
+}
