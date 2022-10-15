2c96da4635118586851e885d8eb283d482243100
Дмитрий <semu-dima@yandex.ru>
2022-10-15 13:10:16 +0300

    to the SciMaterials.Services.Database an extension directory with extension files for the application and the collection service has been added

---
+++ Services/SciMaterials.Services.Database/Extensions/ApplicationExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SciMaterials.DAL.Contexts;
+using SciMaterials.Services.Database.Configuration;
+using SciMaterials.Services.Database.Services;
+
+namespace SciMaterials.Services.Database.Extensions
+{
+    public static class ApplicationExtension
+    {
+        public static async Task<IApplicationBuilder> UseInitializationDbAsync(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            await using var scope = app.ApplicationServices.CreateAsyncScope();
+
+            var dbSetting = configuration.GetSection("DbSettings").Get<DbSettings>();
+
+            if (dbSetting.DbProvider.Equals("SQLite"))
+            {
+                var context = scope.ServiceProvider.GetRequiredService<SciMaterialsContext>();
+                await context.Database.MigrateAsync().ConfigureAwait(false);
+            }
+
+            var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+            await dbInitializer.InitializeDbAsync(removeAtStart: dbSetting.RemoveAtStart, useDataSeeder: dbSetting.UseDataSeeder).ConfigureAwait(false);
+
+            return app;
+        }
+    }
+}
