dfc7eb3bcd1d63f3cf33c5a2917b98b714608665
Orlfi <orlfi@mail.ru>
2022-10-19 14:33:55 +0300

    add categories client

---
+++ Services/SciMaterials.WebApi.Clients/Categories/CategoriesClient.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using SciMaterials.Contracts.API.Constants;
+using SciMaterials.Contracts.API.DTO.Categories;
+
+namespace SciMaterials.WebApi.Clients.Categories;
+
+public class CategoriesClient :
+    ApiClientWithAddBase<CategoriesClient, AddCategoryRequest, EditCategoryRequest, Guid, GetCategoryResponse>,
+    ICategoriesClient
+{
+    public CategoriesClient(HttpClient httpClient, ILogger<CategoriesClient> logger) : base(httpClient, logger)
+        => _webApiRoute = WebApiRoute.Categories;
+}
---
+++ Services/SciMaterials.WebApi.Clients/Categories/ICategoriesClient.cs
@@ -0,0 +1,10 @@
+﻿using SciMaterials.Contracts.API.DTO.Categories;
+
+namespace SciMaterials.WebApi.Clients.Categories;
+
+public interface ICategoriesClient :
+    IApiReadonlyClient<Guid, GetCategoryResponse>,
+    IApiModifyClient<AddCategoryRequest, EditCategoryRequest, Guid>,
+    IApiDeleteClient<Guid>
+{
+}
