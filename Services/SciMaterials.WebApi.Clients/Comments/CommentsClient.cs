7c7bbaf116a4a9d9811087049d98e5b2eabd0c4d
Orlfi <orlfi@mail.ru>
2022-10-19 14:37:10 +0300

    add comments client

---
+++ Services/SciMaterials.WebApi.Clients/Comments/CommentsClient.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using SciMaterials.Contracts.API.Constants;
+using SciMaterials.Contracts.API.DTO.Comments;
+
+namespace SciMaterials.WebApi.Clients.Comments;
+
+public class CommentsClient :
+    ApiClientWithAddBase<CommentsClient, AddCommentRequest, EditCommentRequest, Guid, GetCommentResponse>,
+    ICommentsClient
+{
+    public CommentsClient(HttpClient httpClient, ILogger<CommentsClient> logger) : base(httpClient, logger)
+        => _webApiRoute = WebApiRoute.Comments;
+}
---
+++ Services/SciMaterials.WebApi.Clients/Comments/ICommentsClient.cs
@@ -0,0 +1,10 @@
+﻿using SciMaterials.Contracts.API.DTO.Comments;
+
+namespace SciMaterials.WebApi.Clients.Comments;
+
+public interface ICommentsClient :
+    IApiReadonlyClient<Guid, GetCommentResponse>,
+    IApiModifyClient<AddCommentRequest, EditCommentRequest, Guid>,
+    IApiDeleteClient<Guid>
+{
+}
