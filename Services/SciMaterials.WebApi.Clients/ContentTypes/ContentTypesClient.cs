591966863b768e4410dfded4ca26b7ee650e412a
Orlfi <orlfi@mail.ru>
2022-10-19 16:34:12 +0300

    add ContentTypes client

---
+++ Services/SciMaterials.WebApi.Clients/ContentTypes/ContentTypesClient.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using SciMaterials.Contracts.API.Constants;
+using SciMaterials.Contracts.API.DTO.ContentTypes;
+
+namespace SciMaterials.WebApi.Clients.ContentTypes;
+
+public class ContentTypesClient :
+    ApiClientWithAddBase<ContentTypesClient, AddContentTypeRequest, EditContentTypeRequest, Guid, GetContentTypeResponse>,
+    IContentTypesClient
+{
+    public ContentTypesClient(HttpClient httpClient, ILogger<ContentTypesClient> logger) : base(httpClient, logger)
+        => _webApiRoute = WebApiRoute.ContentTypes;
+}
---
+++ Services/SciMaterials.WebApi.Clients/ContentTypes/IContentTypesClient.cs
@@ -0,0 +1,10 @@
+﻿using SciMaterials.Contracts.API.DTO.ContentTypes;
+
+namespace SciMaterials.WebApi.Clients.ContentTypes;
+
+public interface IContentTypesClient :
+    IApiReadonlyClient<Guid, GetContentTypeResponse>,
+    IApiModifyClient<AddContentTypeRequest, EditContentTypeRequest, Guid>,
+    IApiDeleteClient<Guid>
+{
+}
