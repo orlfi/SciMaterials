4cb1677b4b859c3df7fb030d53817aad7c321bc7
Orlfi <orlfi@mail.ru>
2022-10-19 16:35:28 +0300

    add Tags client

---
+++ Services/SciMaterials.WebApi.Clients/Tags/TagsClient.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using SciMaterials.Contracts.API.Constants;
+using SciMaterials.Contracts.API.DTO.Tags;
+
+namespace SciMaterials.WebApi.Clients.Tags;
+
+public class TagsClient :
+    ApiClientWithAddBase<TagsClient, AddTagRequest, EditTagRequest, Guid, GetTagResponse>,
+    ITagsClient
+{
+    public TagsClient(HttpClient httpClient, ILogger<TagsClient> logger) : base(httpClient, logger)
+        => _webApiRoute = WebApiRoute.Tags;
+}
