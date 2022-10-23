9934c3116b694e1f7e354bc03bec898ecae62780
Tarxos <ftarxos@gmail.com>
2022-10-23 03:19:39 +0300

    clear upload file details form model, as work with it will be reworked

--- UI/SciMaterials.UI.BWASM/Models/UploadFileDetailsForm.cs
+++ UI/SciMaterials.UI.BWASM/Models/UploadFileDetailsForm.cs
@@ -2,27 +2,10 @@
 
 public class UploadFileDetailsForm
 {
-    public static UploadFileDetailsForm Empty { get; } = new UploadFileDetailsForm();
+    public static readonly UploadFileDetailsForm Empty = new UploadFileDetailsForm();
 
-    public Guid Id { get; private init; }
+    public Guid Id { get; init; }
     public string FileName { get; set; } = null!;
     public string? Category { get; set; }
-    public long Size { get; private init; }
-
-    public void UpdateSource(FileUploadData? source)
-    {
-        // source may be already deleted! TODO: why it already not handled on delete?
-        if (source is null) return;
-
-        source.FileName = FileName;
-        source.Category = Category;
-    }
-
-    public static implicit operator UploadFileDetailsForm(FileUploadData data) => new()
-    {
-        Id = data.Id,
-        FileName = data.FileName,
-        Category = data.Category,
-        Size = data.File.Size
-    };
+    public long Size { get; init; }
 }
