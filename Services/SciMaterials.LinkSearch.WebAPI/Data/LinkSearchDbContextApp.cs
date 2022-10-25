ff6546e589e33d88ca7c1639f60bc7636f3f42ca
Konstantin <muselsps@ya.ru>
2022-10-25 14:24:28 +0300

    LinkSearchWebAPI

---
+++ Services/SciMaterials.LinkSearch.WebAPI/Data/LinkSearchDbContextApp.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SciMaterials.LinkSearch.WebAPI.Data
+{
+    public class LinkSearchDbContextApp : DbContext
+    {
+        public DbSet<Models.LinkSearch> LinkSearches { get; set; }
+
+        public LinkSearchDbContextApp(DbContextOptions options) : base(options) { }
+    }
+}
