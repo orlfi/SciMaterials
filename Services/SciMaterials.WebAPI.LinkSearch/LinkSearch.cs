dae2652a749e709bb4536fb53edfa8327da474d0
Konstantin <muselsps@ya.ru>
2022-10-25 17:20:45 +0300

    LinkSearchNew

---
+++ Services/SciMaterials.WebAPI.LinkSearch/LinkSearch.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+using SciMaterials.Contracts.WebAPI.LinkSearch;
+
+namespace SciMaterials.WebAPI.LinkSearch;
+
+public class LinkSearch : ILinkSearch
+{
+    public string ParsingLink(string text)
+    {
+        return Regex.Replace(text,
+            @"((http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)",
+            "\"$1\"");
+    }
+}
