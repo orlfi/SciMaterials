f5c295348693015ebcaea6660dd6595e48ac8b4c
Дмитрий <semu-dima@yandex.ru>
2022-10-26 11:40:52 +0300

    removed properties in the link model

--- Data/SciMaterials.DAL/Models/Link.cs
+++ Data/SciMaterials.DAL/Models/Link.cs
@@ -9,9 +9,6 @@ namespace SciMaterials.DAL.Models
         public string SourceAddress { get; set; } = string.Empty;
         public string? Hash { get; set; }
         public Guid AuthorId { get; set; }
-        public DateTime LastAccessedDate { get; set; }
-        public int NumberOfRequest { get; set; } = 0;
-        [Timestamp] public byte[] TimeStamp { get; set; } = null!;
         public string? Description { get; set; }
 
         public Author Author { get; set; } = null!;
