2873dc7242005d2f5f61aca80d33005b7fe57675
Tarxos <ftarxos@gmail.com>
2022-10-26 03:45:24 +0300

    fix role claims parsing from jwt

--- UI/SciMaterials.UI.BWASM/Extensions/JwtStringToClaimsExtension.cs
+++ UI/SciMaterials.UI.BWASM/Extensions/JwtStringToClaimsExtension.cs
@@ -8,6 +8,15 @@ public static class JwtStringToClaimsExtension
 {
     public static IReadOnlyCollection<Claim> ParseJwt(this string jwt)
     {
-        return new JwtSecurityTokenHandler().ReadJwtToken(jwt).Claims.ToList();
+        var claims = new JwtSecurityTokenHandler().ReadJwtToken(jwt).Claims.ToList();
+        if (claims.Where(x => x.Type == "role").ToArray() is {Length: > 0 } roleClaims)
+        {
+            foreach (Claim roleClaim in roleClaims)
+            {
+                var index = claims.IndexOf(roleClaim);
+                claims[index] = new Claim(ClaimTypes.Role, roleClaim.Value);
+            }
+        }
+        return claims;
     }
 }
