ad1a5c5b0d2b9937c0215ed8e3107f0109ce2f52
Orlfi <orlfi@mail.ru>
2022-10-28 09:59:15 +0300

    Replace Title to FileInfo

--- Services/SciMaterials.Contracts.API/DTO/Files/GetFileResponse.cs
+++ Services/SciMaterials.Contracts.API/DTO/Files/GetFileResponse.cs
@@ -4,7 +4,7 @@ public class GetFileResponse
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
+    public string ShortInfo { get; set; } = string.Empty;
     public string? Description { get; set; }
     public string? Url { get; set; }
     public long Size { get; set; }
--- Services/SciMaterials.Contracts.API/DTO/Files/UploadFileRequest.cs
+++ Services/SciMaterials.Contracts.API/DTO/Files/UploadFileRequest.cs
@@ -3,7 +3,7 @@ namespace SciMaterials.Contracts.API.DTO.Files;
 public class UploadFileRequest
 {
     public string Name { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
+    public string ShortInfo { get; set; } = string.Empty;
     public string? Description { get; set; }
     public long Size { get; set; }
     public string ContentTypeName { get; set; } = string.Empty;
