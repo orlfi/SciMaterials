41e943379c99ca2d4a9f9d12d8568bb0265a5ccc
orlfi <orlfi@mail.ru>
2022-10-31 22:48:32 +0300

    Change null to string default value in text fields

--- Data/SciMaterials.DAL/Models/Comment.cs
+++ Data/SciMaterials.DAL/Models/Comment.cs
@@ -6,7 +6,7 @@ public class Comment : BaseModel
 {
     public Guid? ParentId { get; set; }
     public Guid AuthorId { get; set; }
-    public string Text { get; set; } = null!;
+    public string Text { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
     
     public Guid? FileId { get; set; }
