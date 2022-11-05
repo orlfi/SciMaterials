3f84f9f77ab0955423cc887d0e135feaa52b1822
Дмитрий <semu-dima@yandex.ru>
2022-11-05 20:07:53 +0300

    added Enum with names of assemblies with migrations

---
+++ Services/SciMaterials.Services.Database/Enums/Migrations.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace SciMaterials.Services.Database.Enums
+{
+    public enum Migrations : byte
+    {
+        [Description("SciMaterials.MsSqlServerMigrations")]
+        SqlServer,
+
+        [Description("SciMaterials.PostgresqlMigrations")]
+        PostgreSQL,
+
+        [Description("SciMaterials.Data.MySqlMigrations")]
+        MySQL,
+
+        [Description("SciMaterials.SQLiteMigrations")]
+        SQLite
+    }
+}
