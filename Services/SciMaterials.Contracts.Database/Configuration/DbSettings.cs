40805271898bd30ede91e78745bc9ef69cd2e78e
Дмитрий <semu-dima@yandex.ru>
2022-11-06 22:58:23 +0300

    removing unnecessary methods

--- Services/SciMaterials.Contracts.Database/Configuration/DbSettings.cs
+++ Services/SciMaterials.Contracts.Database/Configuration/DbSettings.cs
@@ -6,10 +6,4 @@ public sealed class DbSettings
     public string DbProvider { get; init; }
     public bool RemoveAtStart { get; init; }
     public bool UseDataSeeder { get; init; }
-
-    public string GetProviderName()
-        => DbProvider.Split(".", 2, StringSplitOptions.RemoveEmptyEntries)[0];
-
-    public string GetConnectionString()
-        => DbProvider.Split(".", 2, StringSplitOptions.RemoveEmptyEntries)[1];
 }
