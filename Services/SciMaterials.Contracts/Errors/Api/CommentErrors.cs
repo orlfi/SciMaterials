6ceb410e7269ba9247fa55e198ec5ff120c35a10
orlfi <orlfi@mail.ru>
2022-11-10 21:01:57 +0300

    add error codes for content type service

--- Services/SciMaterials.Contracts/Errors/Api/CommentErrors.cs
+++ Services/SciMaterials.Contracts/Errors/Api/CommentErrors.cs
@@ -5,13 +5,13 @@ namespace SciMaterials.Contracts.Errors.Api;
 /// <summary> Ошибки API (10000-19999). </summary>
 public static partial class ApiErrors
 {
-    /// <summary> Ошибки сервиса CommentService (10100-10199). </summary>
+    /// <summary> Ошибки сервиса CommentService (10300-10399). </summary>
     public static class Comment
     {
-        public static readonly Error NotFound = new(10100, "Comment not found");
-        public static readonly Error Add = new(10101, "Comment add error");
-        public static readonly Error Update = new(10102, "Comment update error");
-        public static readonly Error Delete = new(10103, "Comment delete error");
-        public static readonly Error Exist = new(10104, "Comment already exist");
+        public static readonly Error NotFound = new(10300, "Comment not found");
+        public static readonly Error Add = new(10301, "Comment add error");
+        public static readonly Error Update = new(10302, "Comment update error");
+        public static readonly Error Delete = new(10303, "Comment delete error");
+        public static readonly Error Exist = new(10304, "Comment already exist");
     }
 }
