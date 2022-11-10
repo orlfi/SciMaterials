1f220d0826b096ce60c8fd77288218e47a62c4ba
orlfi <orlfi@mail.ru>
2022-11-10 21:03:30 +0300

    add error codes for tag service

---
+++ Services/SciMaterials.Contracts/Errors/Api/TagErrors.cs
@@ -0,0 +1,17 @@
+﻿using SciMaterials.Contracts.Result;
+
+namespace SciMaterials.Contracts.Errors.Api;
+
+/// <summary> Ошибки API (10000-19999). </summary>
+public static partial class ApiErrors
+{
+    /// <summary> Ошибки сервиса TagService (10500-10599). </summary>
+    public static class Tag
+    {
+        public static readonly Error NotFound = new(10500, "Tag not found");
+        public static readonly Error Add = new(10501, "Tag add error");
+        public static readonly Error Update = new(10502, "Tag update error");
+        public static readonly Error Delete = new(10503, "Tag delete error");
+        public static readonly Error Exist = new(10504, "Tag already exist");
+    }
+}
