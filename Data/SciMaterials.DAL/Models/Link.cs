0d3a1d89ef5ae7f7f841ec8c7cc156800152db65
Orlfi <orlfi@mail.ru>
2022-11-16 12:06:43 +0300

    Change type of LastAccess in Link model

--- Data/SciMaterials.DAL/Models/Link.cs
+++ Data/SciMaterials.DAL/Models/Link.cs
@@ -7,6 +7,6 @@ public class Link : BaseModel
     public string SourceAddress { get; set; } = null!;
     public string Hash { get; set; } = null!;
     public int AccessCount { get; set; }
-    public int LastAccess { get; set; }
+    public DateTime? LastAccess { get; set; }
     public byte[] RowVersion { get; set; } = null!;
 }
