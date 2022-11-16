f14befed7414265aa1034023ca8f38fc28c282f4
Orlfi <orlfi@mail.ru>
2022-11-16 14:15:53 +0300

    Add MS SQL Migration  Link_Model_Change_LastAccess_Type

--- Data/Migrations/SciMaterials.MsSqlServerMigrations/Migrations/20221116101911_Link_Model_Change_LastAccess_Type.Designer.cs
+++ Data/Migrations/SciMaterials.MsSqlServerMigrations/Migrations/20221116101911_Link_Model_Change_LastAccess_Type.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SciMaterials.DAL.Contexts;
 
@@ -11,9 +12,10 @@ using SciMaterials.DAL.Contexts;
 namespace SciMaterials.MsSqlServerMigrations.Migrations
 {
     [DbContext(typeof(SciMaterialsContext))]
-    partial class SciMaterialsContextModelSnapshot : ModelSnapshot
+    [Migration("20221116101911_Link_Model_Change_LastAccess_Type")]
+    partial class Link_Model_Change_LastAccess_Type
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
