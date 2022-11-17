75b514c3529ce0d25310d1fbdbc36ca034c9a849
orlfi <orlfi@mail.ru>
2022-11-18 00:45:19 +0300

    Add urls grid

--- Services/SciMaterials.Contracts.API/DTO/Urls/GetUrlResponse.cs
+++ Services/SciMaterials.Contracts.API/DTO/Urls/GetUrlResponse.cs
@@ -6,9 +6,7 @@ public class GetUrlResponse
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public Guid? ParentId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; }
     public string Link { get; set; } = null!;
-    public Guid? ContentTypeId { get; set; }
 }
---
+++ Services/SciMaterials.Contracts.WebApi.Clients/Urls/IUrlsClient.cs
@@ -0,0 +1,10 @@
+﻿using SciMaterials.Contracts.API.DTO.Urls;
+
+namespace SciMaterials.Contracts.WebApi.Clients.Urls;
+
+public interface IUrlsClient :
+    IApiReadonlyClient<Guid, GetUrlResponse>,
+    IApiModifyClient<Guid, AddUrlRequest, EditUrlRequest>,
+    IApiDeleteClient<Guid>
+{
+}
