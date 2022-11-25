0f3bddce7cde99813193e0f2807c5906582cfded
orlfi <orlfi@mail.ru>
2022-11-25 19:30:15 +0300

    Modify Resource model

--- Data/SciMaterials.DAL/Models/Base/Resource.cs
+++ Data/SciMaterials.DAL/Models/Base/Resource.cs
@@ -5,14 +5,14 @@ using SciMaterials.Domain.Enums;
 namespace SciMaterials.DAL.Models.Base;
 
 [Table("Resources")]
-public abstract class Resource : NamedModel
+public class Resource : NamedModel
 {
     public string? ShortInfo { get; set; }
     public string? Description { get; set; }
     public Guid AuthorId { get; set; }
     public DateTime CreatedAt { get; set; }
     public Author Author { get; set; } = null!;
-    public abstract ResourceType ResourceType { get; }
+    public virtual ResourceType ResourceType { get; }
 
     public ICollection<Comment>? Comments { get; set; } = new HashSet<Comment>();
     public ICollection<Tag>? Tags { get; set; } = new HashSet<Tag>();
