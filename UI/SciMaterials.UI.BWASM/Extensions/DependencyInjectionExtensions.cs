59efa72af4325f2e03cb77f8109e2467ac8c807e
Tarxos <ftarxos@gmail.com>
2022-11-28 02:09:10 +0300

    add extension that will register http client and it implementation as scoped service

--- UI/SciMaterials.UI.BWASM/Extensions/DependencyInjectionExtensions.cs
+++ UI/SciMaterials.UI.BWASM/Extensions/DependencyInjectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿// ReSharper disable once CheckNamespace
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+
+// ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DependencyInjectionExtensions
@@ -11,6 +13,18 @@ public static class DependencyInjectionExtensions
         where TImplementation : class, TClient
     {
         var builder = services.AddHttpClient<TClient, TImplementation>(c => c.BaseAddress = new Uri(routeBase));
+        clientConfiguration?.Invoke(builder);
+        return services;
+    }
+
+    public static IServiceCollection AddApiClient<TClient>(
+        this IServiceCollection services,
+        string routeBase,
+        Action<IHttpClientBuilder>? clientConfiguration = null)
+        where TClient : class
+    {
+        services.TryAddScoped<TClient>();
+        var builder = services.AddHttpClient<TClient>(c => c.BaseAddress = new Uri(routeBase));
         clientConfiguration?.Invoke(builder);
         return services;
     }
